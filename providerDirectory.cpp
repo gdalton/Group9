8f2790d21686db18323a05a6bda15e8da0517f1b
Matthew Greenlaw <walneergwehttam@googlemail.com>
2017-11-26 15:18:53 -0800

    Function skeletons added

--- providerDirectory.cpp
+++ providerDirectory.cpp
@@ -8,7 +8,56 @@
  */
 
 #include "mainHeader.h"
+	
 
+    providerDirectory :: providerDirectory ( void ) {}
+    providerDirectory :: providerDirectory ( const providerDirectory & toCopy ) { //@todo
 
+    }
 
+    providerDirectory :: ~providerDirectory ( void ) {}
 
+    /**
+     * Gets the directory.
+     * @return     The directory.
+     */
+    list < Service > providerDirectory :: getDirectory ( void ) {
+
+    }
+
+    /**
+     * Adds a service.
+     * @param      addedService  The added service
+     * @return     True if the service was added, false otherwise
+     */
+    bool providerDirectory :: addService ( Service addedService) {
+
+    }
+
+    /**
+     * Removes a service.
+     * @param      serviceID  The service id
+     * @return     True if the service was removed, false otherwise
+     */
+    bool providerDirectory :: removeService ( int serviceID) {
+
+    }
+
+    /**
+     * Prints service to console
+     * @param      serviceID  The service id
+     * @return     True if the service was displayed, false otherwise (Why is this a bool?)
+     */
+    bool providerDirectory :: displayService ( int serviceID) {
+
+    }
+
+    /**
+     * Updates a service fee
+     * @param      serviceID  The service id
+     * @param      newFee     The new fee
+     * @return     True if the service fee was updated, false otherwise
+     */
+    bool providerDirectory :: updateFee ( int serviceID, float newFee) {
+
+    }
