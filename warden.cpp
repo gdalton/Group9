a3b14744a4af57d8e6194b7b1eaefce087840076
Matthew Greenlaw <walneergwehttam@googlemail.com>
2017-11-26 16:08:27 -0800

    Comment updates

--- warden.cpp
+++ warden.cpp
@@ -9,65 +9,68 @@
 
 #include "mainHeader.h"
 
-    /** Constructs the object.
-     */
-    Warden :: Warden ( void ) { //@todo
+/** Constructs the object.
+ */
+Warden :: Warden ( void ) { //@todo
 
-    }
-   
-    /** Constructs the object.
-     * @param      toCopy  To copy
-     */
-    Warden :: Warden ( const Warden & toCopy ) { //@todo
+}
 
-    }
-    
-    /** Destroys the object.
-     */
-    Warden :: ~Warden ( void ) { //@todo
+/** Constructs the object.
+ * @param      toCopy  To copy
+ */
+Warden :: Warden ( const Warden & toCopy ) { //@todo
 
-    }
-    
-    /** Adds a credential.
-     * @param      newUsername  The new username
-     * @param      newPassword  The new password
-     * @return     Adds a username/password pair to the list of credentials
-     */
-    bool Warden :: addCredential ( string newUsername, string newPassword) { //@todo
+}
 
-    }
-    
-    /** Validates a login.
-     * @param      checkedUsername  The checked username
-     * @param      checkedPassword  The checked password
-     * @return     True if the username/passowrd match
-     */
-    bool Warden :: validateLogin ( string checkedUsername, string checkedPassword) { //@todo
+/** Destroys the object.
+ */
+Warden :: ~Warden ( void ) { //@todo
 
-    }
-    
-    /** Checks how many times a user has failed logins
-     * @param      userID  The user id
-     * @return     True if the user has not exceeded their login attempts, false
-     *             otherwise
-     */
-    bool Warden :: checkLoginAttempts ( int userID) { //@todo
+}
 
-    }
-    
-    /**
-     * Adds a log.
-     * @param      logToAdd  The log to add
-     * @return     True if the log was added, false otherwise
-     */
-    bool Warden :: addLog ( string logToAdd) { //@todo
+/** Adds a credential to systemCredentials.
+ * @param      newUsername  The new username
+ * @param      newPassword  The new password
+ * @return     Adds a username/password pair to the list of credentials
+ */
+bool Warden :: addCredential ( string newUsername, string newPassword) { //@todo
 
-    }
-    
-    /**
-     * Checks if a user is active
-     * @return     True if the user is active, false otherwise
-     */
-    bool Warden :: checkActive ( void ) { //@todo
+}
 
-    }
+/** Returns true if there is a match between the userID / userPassword pair and a
+ * pair in the systemCredentials list
+ * @param      checkedUsername  The checked username
+ * @param      checkedPassword  The checked password
+ * @return     True if the username/passowrd match
+ */
+bool Warden :: validateLogin ( string checkedUsername, string checkedPassword) { //@todo
+
+}
+
+/** Checks how many times a user has failed logins
+ * @param      userID  The user id
+ * @return     Returns true if login attempts < 3., false
+ *             otherwise
+ */
+bool Warden :: checkLoginAttempts ( int userID) { //@todo
+
+}
+
+/**
+ * Adds a log entry to the logFile.
+ @param      logToAdd  The log to add
+ @return     Returns true if the logEntry is successfully entered in the
+             logFile, false otherwise
+*/
+bool Warden :: addLog ( string logToAdd) { //@todo
+
+}
+
+
+/** Checks if the difference between currentTime and referenceTime is <5min
+ * @return     Returns true if the difference between currentTime and
+ *             referenceTime is <5min, false otherwise
+ */
+bool Warden :: checkActive ( void ) { //@todo
+
+}
