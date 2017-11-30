20d0ddb0f697fe3c0aedd0649f0c327b1b36d21f
Katie <phoenixteam20@gmail.com>
2017-11-30 00:11:29 -0800

    Fix unit tests

--- tests/accountTest.cc
+++ tests/accountTest.cc
@@ -8,12 +8,11 @@ TEST(accountTesting, allUsersExistInTable) {
     ifstream allAccounts;
     ifstream account;
     allAccounts.open("../accounts/allIDs.txt");
-
-    while(allAccounts)
+    ASSERT_TRUE(allAccounts) << "Failed to open allIDs.txt" << endl;
+    while(!allAccounts.eof())
     {
         char accountID [20];
-        allAccounts.get(accountID, 20,'\n');
-        allAccounts.get();
+        allAccounts.getline(accountID, 20, '\n');
         ACCOUNT_TYPE type = init;
         if(accountID[0] == '1'){
             type=manager;
@@ -23,27 +22,31 @@ TEST(accountTesting, allUsersExistInTable) {
         }
         else type = member;
 
-        char temp [50] = "accounts/";
+        char temp [60] = "../accounts/";
         strcat(temp,accountID);
         account.open(temp);//Dynamically opens file
-        
+        ASSERT_TRUE(account) << "Failed to open " << temp;
         //Stop if account not found
         if(!account) continue;
         
-        account.get(accountID, 20, '\n');
+        account.getline(accountID, 20, '\n');
         account.get();
 
         string * stringAccountID = new string(accountID);
-        
         ASSERT_TRUE(testAccountManager.getAccount(stringAccountID, type) != NULL) << "Account failed to load " << accountID;
         delete stringAccountID;
     }
 }
 
 TEST(accountTesting, canLogin) {
+    
     accountManager testAccountManager = accountManager();
-    string userID = string("122475252");
-    Account * account = testAccountManager.getAccount(&userID, ( ACCOUNT_TYPE ) ( userID.at ( 0 ) - '0' ) );
-    ASSERT_TRUE(testAccountManager.getAccount(&userID, provider) != NULL) << "Account failed to load " << userID;
-    ASSERT_TRUE(account != NULL) << "Failed to login to test account " << "127850894";
+    string * userID = new string("224938990");
+
+    ACCOUNT_TYPE type = ( ACCOUNT_TYPE ) ( userID->at ( 0 ) - '0' );
+    Account * account = testAccountManager.getAccount(userID, type);
+
+    ASSERT_TRUE(type == provider) << "Failed to get proper user type" << endl;
+    ASSERT_TRUE(account != NULL) << "Failed to login to test account " << *userID;
+    delete userID;
 }
