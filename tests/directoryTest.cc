670369d1c5b66e7a29abb34eabac805dbe922731
Dalton Gray <gdalton@ada.cs.pdx.edu>
2017-12-01 16:22:29 -0800

    Added updateFee test to directoryTest.cc

--- tests/directoryTest.cc
+++ tests/directoryTest.cc
@@ -12,10 +12,18 @@ TEST(unitTests, removeService) {
     toAdd.serviceID = testServiceID;
 
     testDirectory.addService(toAdd);
+    list<Service> directory = testDirectory.getDirectory();
+
+    bool found = false;
+    for(list<Service>::iterator i = directory.begin(); i != directory.end(); ++i) {
+        if(i -> name == testName)
+            found = true;
+    }
+    ASSERT_TRUE(found) << "Failed to add test service";
+
     testDirectory.removeService(testServiceID);
 
-    list<Service> directory = testDirectory.getDirectory();
-    bool found = true; 
+    directory = testDirectory.getDirectory();
 
     for(list<Service>::iterator i = directory.begin(); i != directory.end(); ++i) {
         if(i -> name == testName)
@@ -24,3 +32,34 @@ TEST(unitTests, removeService) {
     ASSERT_TRUE(found) << "Failed to remove test service";
 }    
 
+TEST(unitTests, updateFee) {
+    providerDirectory testDirectory = providerDirectory();
+    string testServiceID = string("111222");
+    string testName = string("TEST SERVICE");
+    Service toAdd;
+
+    toAdd.fee = 0;
+    toAdd.name = testName;
+    toAdd.serviceID = testServiceID;
+
+    testDirectory.addService(toAdd);
+    list<Service> directory = testDirectory.getDirectory();
+
+    bool found = false;
+    for(list<Service>::iterator i = directory.begin(); i != directory.end(); ++i) {
+        if(i -> name == testName)
+            found = true;
+    }
+    ASSERT_TRUE(found) << "Failed to add test service";
+
+    testDirectory.updateFee(testServiceID, 50.5);
+
+    directory = testDirectory.getDirectory();
+    found = false;
+
+    for(list<Service>::iterator i = directory.begin(); i != directory.end(); ++i) {
+        if(i -> name == testName && i -> fee == 50.5)
+            found = true;
+    }
+    ASSERT_TRUE(found) << "Failed to update test service fee";
+}   
